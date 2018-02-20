42f5c896a3f47c6e96b0176a7849d69fccac5409
Puwka <gorokhov.inc@gmail.com>
2018-02-20 22:11:36 +0300

    added server dummy

--- pages/index.vue
+++ pages/index.vue
@@ -14,10 +14,32 @@
         components: {
             PostList
         },
-        asyncData() {
-            return {
-
-            }
+        asyncData(context, callback) {
+          console.log('asyncData is executed')
+          setTimeout(() => {
+              callback(null, {
+                loadedPosts: [
+                  {
+                      id: 1,
+                      title: 'Development',
+                      previewText: 'Learn to Code!',
+                      thumbnail: 'http://www.umbrellaconsultants.com/files/resources/outer-banks-web-development-hosting.jpg'
+                  },
+                  {
+                      id: 2,
+                      title: 'Design',
+                      previewText: 'Learn to Design!',
+                      thumbnail: 'http://www.sparkz.co.in/images/slider/slider3.png'
+                  },
+                  {
+                      id: 3,
+                      title: 'Meetups',
+                      previewText: 'Learn to Talk!',
+                      thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTU-1cth7VWW-qlAFa9xLmO7VV5uiYbJ5lVI-87sk-x4tpEuQ1tQQ'
+                  }
+                ]
+              })
+          }, 1500)
         },
         // data() {
         //     return {
@@ -25,28 +47,7 @@
         //     }
         // },
         created() {
-            setTimeout(() => {
-                this.loadedPosts = [
-                    {
-                        id: 1,
-                        title: 'Development',
-                        previewText: 'Learn to Code!',
-                        thumbnail: 'http://www.umbrellaconsultants.com/files/resources/outer-banks-web-development-hosting.jpg'
-                    },
-                    {
-                        id: 2,
-                        title: 'Design',
-                        previewText: 'Learn to Design!',
-                        thumbnail: 'http://www.sparkz.co.in/images/slider/slider3.png'
-                    },
-                    {
-                        id: 3,
-                        title: 'Meetups',
-                        previewText: 'Learn to Talk!',
-                        thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTU-1cth7VWW-qlAFa9xLmO7VV5uiYbJ5lVI-87sk-x4tpEuQ1tQQ'
-                    }
-                ]
-            }, 1500)
+
         }
     }
 </script>
@@ -94,4 +95,3 @@
 
 
 </style>
-
--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="posts-page">
-        <PostList/>
+        <PostList :posts="loadedPosts"/>
     </div>
 </template>
 
@@ -10,6 +10,32 @@
     export default {
         components: {
             PostList
+        },
+        asyncData(context, callback) {
+          setTimeout(() => {
+            callback(null, {
+              loadedPosts: [
+                {
+                    id: 1,
+                    title: 'Development',
+                    previewText: 'Learn to Code!',
+                    thumbnail: 'http://www.umbrellaconsultants.com/files/resources/outer-banks-web-development-hosting.jpg'
+                },
+                {
+                    id: 2,
+                    title: 'Design',
+                    previewText: 'Learn to Design!',
+                    thumbnail: 'http://www.sparkz.co.in/images/slider/slider3.png'
+                },
+                {
+                    id: 3,
+                    title: 'Meetups',
+                    previewText: 'Learn to Talk!',
+                    thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTU-1cth7VWW-qlAFa9xLmO7VV5uiYbJ5lVI-87sk-x4tpEuQ1tQQ'
+                }
+              ]
+            })
+          }, 1500)
         }
     }
 </script>
